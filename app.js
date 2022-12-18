c9eedea994e8170a016da52b6df7d24b0b9d74fa
C. Felix Lee <felixlee.music@gmail.com>
2022-12-18 01:59:41 -0500

    coding out tetris shapes

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartButton = document.querySelector('#start-button');
+  const width = 10;
+
+  //the tetraminos
+  const lTetramino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTetramino = [
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+  ];
+
+  const tTetramino = [
+    [width, 1, width + 1, width + 2],
+    [1, width + 1, width * 2 + 1, width + 2],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [width, 1, width + 1, width * 2 + 1],
+  ];
+
+  const sqTetramino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+  const iTetramino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+  squares.forEach((square, idx) => {
+    square.innerHTML = idx;
+  });
+});
